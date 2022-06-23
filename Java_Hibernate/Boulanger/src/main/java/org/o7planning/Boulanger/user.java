84943a953af750b962c2860fc7a226245269c8f6
Axel Olivier <axelolivier59@gmail.com>
2022-06-23 13:49:08 +0200

    first commit

---
+++ Java_Hibernate/Boulanger/src/main/java/org/o7planning/Boulanger/user.java
@@ -0,0 +1,61 @@
+package org.o7planning.Boulanger;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "client")
+public class user {
+    @Id
+    @GeneratedValue
+    private int id;
+    @Column(name = "nomClient")
+    private String nom;
+    private String prenom;
+    private String telephone;
+
+    public user() {
+    }
+
+    public user(String nom, String prenom, String telephone) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.telephone = telephone;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone (String telephone) {
+        this.telephone = telephone;
+    }
+    
+}
